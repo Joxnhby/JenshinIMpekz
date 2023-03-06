cb0799f6b2dd0e7c4127cdfed2333af5d06e62ac
Hans <hans.reg888@gmail.com>
2023-03-06 11:06:47 +0700

    modified:   Assets/DvalinxMonstad.png 	modified:   pages/characters/mondstadt.html 	modified:   pages/map.html 	modified:   script/map.js 	modified:   style/character.css 	modified:   style/map.css

---
+++ script/map.js
@@ -0,0 +1,38 @@
+const region = [
+    {
+        name: "Cider Lake",
+        explanation : "A natural freshwater lake. The lake's wide, crystalline appearance has remained unchanged for eons — so clear you can see the bottom, and cool and refreshing to drink. It is none other than these headwaters that brought Mondstadt's ciders and wines to fame and made the lake the symbol of Mondstadt that it is today.",
+        photo : "assets/mondstadt/cider-lake.jpg",
+        map : "monstad"
+    },
+    {
+        name: "Falcon Coast",
+        explanation : "Stretching eastward from Windrise to the sea, Falcon coast — together with Cape Oath — shelters the sea from three sides, creating a bay that sits east of Mondstadt. Given the long coastline and shallow waters, citizens of Mondstadt often come here to enjoy a nice stroll. On occasion, you can see falcons soaring high above the blue waves that brush upon the white sand.",
+        photo : "assets/mondstadt/falcon-coast.jpg",
+        map : "monstad"
+    },
+    {
+        name: "Cape Oath",
+        explanation : "In the southeast corner of Mondstadt's surrounds lies a cape that borders with Galesong Hill. Legend has it two lovers made an oath to each other here and left behind a deeply touching story. To this day the area is still known to be popular amongst young lovebirds. Other than being a great vantage point for watching sunrises and sunsets, the drop in terrain makes it easy to see Windrise, which sits facing the sea.",
+        photo : "assets/mondstadt/cape-oath.jpg",
+        map : "monstad"
+    },
+    {
+        name: "Dawn Winery",
+        explanation : "Nestled in the land southwest of Mondstadt sits the winery that has passed down the legacy of Mondstadt's traditional wines and ciders for generations. The grapes grown here are used to produce a range of wines that are popular right across Teyvat. Every year's products are split, with a portion being sold locally within Mondstadt, the rest is sold off via the trade route that sits south of the city.",
+        photo : "assets/mondstadt/dawn-winery.png",
+        map : "monstad"
+    },
+    {
+        name: "Cider Lake",
+        explanation : "A natural freshwater lake. The lake's wide, crystalline appearance has remained unchanged for eons — so clear you can see the bottom, and cool and refreshing to drink. It is none other than these headwaters that brought Mondstadt's ciders and wines to fame and made the lake the symbol of Mondstadt that it is today.",
+        photo : "assets/mondstadt/cider-lake.jpg",
+        map : "monstad"
+    },
+    {
+        name: "Cider Lake",
+        explanation : "A natural freshwater lake. The lake's wide, crystalline appearance has remained unchanged for eons — so clear you can see the bottom, and cool and refreshing to drink. It is none other than these headwaters that brought Mondstadt's ciders and wines to fame and made the lake the symbol of Mondstadt that it is today.",
+        photo : "assets/mondstadt/cider-lake.jpg",
+        map : "monstad"
+    },
+]
